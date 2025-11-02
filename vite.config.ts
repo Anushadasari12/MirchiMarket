a6acabcc4e88623ef52f43efa1356acc4a6286b5
Anusha Dasari <anusha.dasari@tricecommunity.com>
2025-11-01 22:30:42 -0400

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/MirchiMarket/", // 👈 Add this line — must match your GitHub repo name
+
   server: {
     host: "::",
     port: 8080,
