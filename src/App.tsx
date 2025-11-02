eac21d93d2bba1d31070b610351cea2c422d8f12
Anusha Dasari <anusha.dasari@tricecommunity.com>
2025-11-01 22:50:30 -0400

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -15,12 +15,11 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/MirchiMarket">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
